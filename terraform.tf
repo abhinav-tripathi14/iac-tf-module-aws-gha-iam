a35827566cc0fe2ce912169bc9f965766aaad7ac
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-12-04 18:40:07 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+
+  cloud {
+    organization = "Test-Abhinav"
+
+    workspaces {
+      name = "abc"
+    }
+  }
+
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = ">=0.46.0"
+    }
+  }
+}
