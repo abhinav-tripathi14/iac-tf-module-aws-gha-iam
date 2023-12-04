5665ce3bdc7405d258b8a7efbc17b49cfa9d8b98
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-12-04 18:39:40 +0530

    Create registry.tf

---
+++ registry.tf
@@ -0,0 +1,19 @@
+data "tfe_organization" "foo" {
+  name = "Test-Abhinav"
+}
+
+resource "tfe_registry_module" "registry_module" {
+  #for_each = local.terraform_registry_modules
+  #name     = each.key
+  name = "module-aws-gha-iam"
+  organization = data.tfe_organization.foo.name
+  vcs_repo {
+    display_identifier = "abhinav-tripathi14/iac-tf-module-aws-gha-iam"
+    identifier         = "abhinav-tripathi14/iac-tf-module-aws-gha-iam"
+    oauth_token_id     = var.oauthtokenid
+  }
+}
+
+variable "oauthtokenid" {
+  default = "ot-wsYdmcLgfzBFVnL8"
+}
